37b62858beded7c09963ecc08a85633b70ca3b88
Jakub Wiczkowski <jakubwiczkowsky@gmail.com>
2024-05-08 23:00:47 +0200

    fix: BCHCoder not working for data that is not of specific length

--- coder/bch/BCHCoder.py
+++ coder/bch/BCHCoder.py
@@ -10,10 +10,31 @@ class BCHCoder(Coder):
         self.decoder = komm.BlockDecoder(self.bch_code)
 
     def encode(self, data: list[int]) -> list[int]:
-        return list(self.encoder(data))
+        split_data = self.split_into_chunks(data, self.bch_code.dimension)
+        encoded_data = []
+        for chunk in split_data:
+            for bit in self.encoder(chunk):
+                encoded_data.append(bit)
+        return encoded_data
 
     def decode(self, data: list[int]) -> list[int]:
-        return list(self.decoder(data))
+        split_data = self.split_into_chunks(data, self.bch_code.length)
+        decoded_data = []
+        for chunk in split_data:
+            for bit in self.decoder(chunk):
+                decoded_data.append(bit)
+        return decoded_data
+
+    def split_into_chunks(self, target: list[int], n: int):
+        num_chunks = -(-len(target) // n)  # Equivalent to ceil(len(lst) / n)
+
+        chunks = [target[i * n:(i + 1) * n] for i in range(num_chunks)]
+
+        last_chunk_len = len(chunks[-1])
+        if last_chunk_len < n:
+            chunks[-1].extend([0] * (n - last_chunk_len))
+
+        return chunks
 
     def name(self) -> str:
         return f"BCH"

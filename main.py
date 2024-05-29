feat: add fixed visualisation now it visualise average of results

main.py
@@ -1,5 +1,7 @@
+import glob
 import os
 import time
+import re
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -11,115 +13,183 @@ from coder.triple.TripleCoder import TripleCoder
 from model import BinarySymmetricChannel
 from model import GilbertElliotModel
 
-coders = [TripleCoder(), BCHCoder(mu=5, delta=7), BCHCoder(mu=3, delta=7), ReedSolomonCoder(32), ReedSolomonCoder(64), ReedSolomonCoder(128),
-          ReedSolomonCoder(200)]
-models = [BinarySymmetricChannel(0.5), BinarySymmetricChannel(0.2), BinarySymmetricChannel(0.02), BinarySymmetricChannel(0.002),
-          BinarySymmetricChannel(0.0002), BinarySymmetricChannel(0.00002), GilbertElliotModel(0.1, 0.8, 0.7, 0.01),
-          GilbertElliotModel(0.01, 0.7, 0.7, 0.0001), GilbertElliotModel(0.05, 0.9, 0.9, 0.001)]
+binary_symmetric_channel_good_params = [0.000002]
+binary_symmetric_channel_bad_params = [0.3]
+gilbert_elliot_model_good_params = [0.1, 0.8, 0.9, 0.005]
+gilbert_elliot_model_bad_params = [0.3, 0.6, 0.5, 0.02]
+
+coders = [TripleCoder(), BCHCoder(mu=5, delta=7), BCHCoder(mu=4, delta=7), BCHCoder(10, 11),
+          ReedSolomonCoder(16), ReedSolomonCoder(64), ReedSolomonCoder(128)]
+
+models = [BinarySymmetricChannel(*binary_symmetric_channel_good_params),
+          BinarySymmetricChannel(*binary_symmetric_channel_bad_params),
+          GilbertElliotModel(*gilbert_elliot_model_good_params),
+          GilbertElliotModel(*gilbert_elliot_model_bad_params)]
 
 models.reverse()
 
 np.random.seed(int(time.time()))
 x = list(np.random.randint(2, size=1024))
-# print(f"Oryginalna tablica:     {x}")
+
+output_directory = "output_files"
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 for coder in coders:
-    # print("===========================")
-    # print(f"# Koder {coder.name()}")
-    filepath = coder.name() + ".txt"
-    # print(filepath)
-    file = open(filepath, "a")
+    count = 4
     for channel in models:
-        # print(f" > Kanał {channel.name()}")
-        encoded = coder.encode(x)
-        output = channel.accept(encoded)
-        decoded = coder.decode(output)
-        if (len(decoded) > len(x)):
-            decoded = decoded[0:len(x)]
-        ber = model.check_integrity(x, decoded)
-        excess = len(encoded) / len(x) * 100
-        # print(f"  - org: {x}")
-        # print(f"  - enc: {encoded}")
-        # print(f"  - out: {output}")
-        # print(f"  - dec: {decoded}")
-        # print(f"  - BER: {ber * 100}%")
-        # print(f"  - exc: {excess}%")
-        save = f"{coder.name()};{channel.name()}; {channel.parameters()} {ber * 100}; {excess}; {coder.parameters()}\n"
-        file.writelines(save)
-        # print(save)
-    file.close()
-    #do zapisu: [nazwa kodera; nazwa_kanalu; parametry kanalu; nazwa bit error rate; nadmiar bitowy; parametry kodera]
+        filepath = os.path.join(output_directory,
+                                f"{coder.name()}_{coder.parameters().replace(' ', '').replace(';', '_')}_{count}.txt")
+        with open(filepath, "a") as file:
+            for i in range(50):
+                x = list(np.random.randint(2, size=np.random.randint(1024)))
 
-plt.rcParams["figure.figsize"] = [6, 4]
-plt.rcParams["figure.autolayout"] = True
+                try:
+                    encoded = coder.encode(x)
+                except Exception as e:
+                    print(f"Error: {e}")
+                    continue
 
-data_list = ['BCH.txt', 'Potrojeniowy.txt', 'RS.txt']
+                output = channel.accept(encoded)
+                decoded = coder.decode(output)
+                if len(decoded) > len(x):
+                    decoded = decoded[:len(x)]
 
-root_dir = "plots"
+                try:
+                    ber = model.check_integrity(x, decoded)
+                except Exception as e:
+                    print(e)
+                    continue
 
-for data in data_list:
-    count = 0
-    with open(data, "rt") as file:
+                excess = len(encoded) / len(x) * 100
+
+                save = (f"{coder.name()};{channel.name()}; {ber * 100}; {excess}; {coder.parameters()}; "
+                        f"{channel.parameters()}\n")
+                file.writelines(save)
+            count -= 1
+
+# do zapisu: [nazwa kodera; nazwa_kanalu; nazwa bit error rate; nadmiar bitowy; parametry kanalu; parametry kodera]
+
+results = []
+file_list = glob.glob(os.path.join(output_directory, '*'))
+count_state = 0
+
+for file_path in file_list:
+    with open(file_path, 'r') as file:
+        ber = []
+        exceed = []
+
         for x in file:
             tokens = x.split(';')
+            ber.append(float(tokens[2]))
+            exceed.append(float(tokens[3]))
 
-            if tokens[0] == 'BCH':
-                coder_name = tokens[0]
-                model_name = tokens[1]
-                if tokens[1] == 'BSC':
-                    model_params = [tokens[2]]
-                    ber = tokens[3]
-                    excess = tokens[4]
-                    coder_params = [tokens[5], tokens[6]]
-                else:
-                    model_params = [tokens[2], tokens[3], tokens[4], tokens[5]]
-                    ber = tokens[6]
-                    excess = tokens[7]
-                    coder_params = [tokens[8], tokens[9]]
+        canal_name = tokens[1]
+        coder_name = re.split(r'[\\.]', file_path)[1][:-2]
+        average_ber = sum(ber) / len(ber)
+        average_exceed = sum(exceed) / len(exceed)
+        state = 0 if count_state % 2 == 0 else 1
+        count_state += 1
 
-            if tokens[0] == 'Potrojeniowy':
-                coder_name = tokens[0]
-                model_name = tokens[1]
-                if tokens[1] == 'BSC':
-                    model_params = [tokens[2]]
-                    ber = tokens[3]
-                    excess = tokens[4]
-                    coder_params = []
-                else:
-                    model_params = [tokens[2], tokens[3], tokens[4], tokens[5]]
-                    ber = tokens[6]
-                    excess = tokens[7]
-                    coder_params = []
+        result = {'coder name': coder_name,
+                  'canal name': canal_name,
+                  'average ber': average_ber,
+                  'average exceed': average_exceed,
+                  'state': state}
+        results.append(result)
 
-            if tokens[0] == 'RS':
-                coder_name = tokens[0]
-                model_name = tokens[1]
-                if tokens[1] == 'BSC':
-                    model_params = [tokens[2]]
-                    ber = tokens[3]
-                    excess = tokens[4]
-                    coder_params = [tokens[5]]
-                else:
-                    model_params = [tokens[2], tokens[3], tokens[4], tokens[5]]
-                    ber = tokens[6]
-                    excess = tokens[7]
-                    coder_params = [tokens[8]]
+x_BSC_good = []
+x_BSC_bad = []
+x_Gilbert_good = []
+x_Gilbert_bad = []
+y_BSC_good = []
+y_BSC_bad = []
+y_Gilbert_good = []
+y_Gilbert_bad = []
+titles_BSC_good = []
+titles_BSC_bad = []
+titles_Gilbert_good = []
+titles_Gilbert_bad = []
 
-            coder_params_str = ','.join(coder_params).replace('\n', '')
-            model_params_str = ','.join(model_params).replace('\n', '')
+for result in results:
+    match result.get('canal name'), result.get('state'):
+        case ('BSC', 0):
+            titles_BSC_good.append(result.get('coder name'))
+            x_BSC_good.append(result.get('average ber'))
+            y_BSC_good.append(result.get('average exceed'))
+        case ('BSC', 1):
+            titles_BSC_bad.append(result.get('coder name'))
+            x_BSC_bad.append(result.get('average ber'))
+            y_BSC_bad.append(result.get('average exceed'))
+        case ('Gilbert-Elliot', 0):
+            titles_Gilbert_good.append(result.get('coder name'))
+            x_Gilbert_good.append(result.get('average ber'))
+            y_Gilbert_good.append(result.get('average exceed'))
+        case ('Gilbert-Elliot', 1):
+            titles_Gilbert_bad.append((result.get('coder name')))
+            x_Gilbert_bad.append(result.get('average ber'))
+            y_Gilbert_bad.append(result.get('average exceed'))
 
-            title = f"{coder_name}({coder_params_str})\n{model_name}({model_params_str})"
+print(x_BSC_good)
+print(x_BSC_bad)
+print(titles_BSC_good)
+print(titles_BSC_bad)
 
-            plt.title(title, loc='center', wrap=False)
-            plt.scatter(ber, excess)
+# Plot BST Good State
+plt.figure(figsize=(10, 8))
+plt.scatter(x_BSC_good, y_BSC_good, color='green', label='BST Good State')
+for i, txt in enumerate(titles_BSC_good):
+    plt.annotate(txt, (x_BSC_good[i], y_BSC_good[i]))
+plt.xlabel('Average BER')
+plt.ylabel('Average Exceed')
+plt.title('BST Good State')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-            nested_dir = os.path.join(root_dir, tokens[0])
-            os.makedirs(nested_dir, exist_ok=True)
+# Plot BST Bad State
+plt.figure(figsize=(10, 8))
+plt.scatter(x_BSC_bad, y_BSC_bad, color='red', label='BST Bad State')
+for i, txt in enumerate(titles_BSC_bad):
+    plt.annotate(txt, (x_BSC_bad[i], y_BSC_bad[i]))
+plt.xlabel('Average BER')
+plt.ylabel('Average Exceed')
+plt.title('BST Bad State')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-            filename = os.path.join(nested_dir, f"{tokens[0]}{count}.png")
-            count += 1
+# Plot Gilbert-Elliot Good State
+plt.figure(figsize=(10, 8))
+plt.scatter(x_Gilbert_good, y_Gilbert_good, color='blue', label='Gilbert-Elliot Good State')
+for i, txt in enumerate(titles_Gilbert_good):
+    plt.annotate(txt, (x_Gilbert_good[i], y_Gilbert_good[i]))
+plt.xlabel('Average BER')
+plt.ylabel('Average Exceed')
+plt.title('Gilbert-Elliot Good State')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-            plt.xlabel("Bit Error Rate")
-            plt.ylabel("Excess")
-            plt.savefig(filename)
-            plt.close()
+# Plot Gilbert-Elliot Bad State
+plt.figure(figsize=(10, 8))
+plt.scatter(x_Gilbert_bad, y_Gilbert_bad, color='orange', label='Gilbert-Elliot Bad State')
+for i, txt in enumerate(titles_Gilbert_bad):
+    plt.annotate(txt, (x_Gilbert_bad[i], y_Gilbert_bad[i]))
+plt.xlabel('Average BER')
+plt.ylabel('Average Exceed')
+plt.title('Gilbert-Elliot Bad State')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+for file_path in file_list:
+    os.remove(file_path)
+
+
+
+
+
+
+
+
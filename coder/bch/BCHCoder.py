e1aad0f6f497533a0676d4ad4ef339f6554464bd
Jakub Wiczkowski <jakubwiczkowsky@gmail.com>
2024-06-18 21:30:19 +0200

    fix: bch coder (possibly again)

--- coder/bch/BCHCoder.py
+++ coder/bch/BCHCoder.py
@@ -11,21 +11,21 @@ class BCHCoder(Coder):
         self.decoder = komm.BlockDecoder(self.bch_code)
 
     def encode(self, data: list[int]) -> list[int]:
-        # split_data = self.split_into_chunks(data, self.bch_code.dimension)
+        split_data = self.split_into_chunks(data, self.bch_code.dimension)
         # print(self.bch_code.dimension)
-        # encoded_data = []
-        # for chunk in split_data:
-        #     for bit in self.encoder(chunk):
-        #         encoded_data.append(bit)
-        return self.encoder(data).tolist()
+        encoded_data = []
+        for chunk in split_data:
+            for bit in self.encoder(chunk):
+                encoded_data.append(bit)
+        return encoded_data
 
     def decode(self, data: list[int]) -> list[int]:
-        # split_data = self.split_into_chunks(data, self.bch_code.length)
-        # decoded_data = []
-        # for chunk in split_data:
-        #     for bit in self.decoder(chunk):
-        #         decoded_data.append(bit)
-        return self.decoder(data).tolist()
+        split_data = self.split_into_chunks(data, self.bch_code.length)
+        decoded_data = []
+        for chunk in split_data:
+            for bit in self.decoder(chunk):
+                decoded_data.append(bit)
+        return decoded_data
 
     def split_into_chunks(self, target: list[int], n: int):
         num_chunks = -(-len(target) // n)  # Equivalent to ceil(len(lst) / n)

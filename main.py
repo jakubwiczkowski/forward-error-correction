8a0dd234029afdc258ad24572de79659722eb843
Robertas Vasiliauskas <robertaswasiliauskas@gmail.com>
2024-06-19 19:09:56 +0200

    fix: bud was found then bad state was plotted as good

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from coder.triple.TripleCoder import TripleCoder
 from model import BinarySymmetricChannel
 from model import GilbertElliotModel
 
-binary_symmetric_channel_good_params = [0.000002]
+binary_symmetric_channel_good_params = [0.0005]
 binary_symmetric_channel_bad_params = [0.3]
 gilbert_elliot_model_good_params = [0.1, 0.8, 0.9, 0.005]
 gilbert_elliot_model_bad_params = [0.3, 0.6, 0.5, 0.02]
@@ -40,39 +40,41 @@ for coder in coders:
         filepath = os.path.join(output_directory,
                                 f"{coder.name()}_{coder.parameters().replace(' ', '').replace(';', '_')}_{count}.txt")
         with open(filepath, "a") as file:
-            x = list(np.random.randint(2, size=np.random.randint(1048576)))
-            prepared = coder.prepare(x)
+            for i in range(1):
+                x = list(np.random.randint(2, size=np.random.randint(2048)))
+                prepared = coder.prepare(x)
 
-            try:
-                encoded = coder.encode(prepared)
-            except Exception as e:
-                print(f"Error: {e}")
-                continue
+                try:
+                    encoded = coder.encode(prepared)
+                except Exception as e:
+                    print(f"Error: {e}")
+                    continue
 
-            output = channel.accept(encoded)
-            decoded = coder.decode(output)
-            if len(decoded) > len(prepared):
-                decoded = decoded[:len(prepared)]
+                output = channel.accept(encoded)
+                decoded = coder.decode(output)
 
-            try:
-                 ber = model.check_integrity(prepared, decoded)
-            except Exception as e:
-                print(e)
-                continue
+                if len(decoded) > len(prepared):
+                    decoded = decoded[:len(prepared)]
 
-            excess = len(encoded) / len(x) * 100
+                try:
+                    ber = model.check_integrity(prepared, decoded)
 
-            save = (f"{coder.name()};{channel.name()}; {ber * 100}; {excess}; {coder.parameters()}; "
+                except Exception as e:
+                    print(e)
+                    continue
+
+                excess = len(encoded) / len(x) * 100
+
+                save = (f"{coder.name()};{channel.name()}; {ber * 100}; {excess}; {coder.parameters()}; "
                         f"{channel.parameters()}\n")
-            file.writelines(save)
+                file.writelines(save)
             count -= 1
-            print('done')
 
 # do zapisu: [nazwa kodera; nazwa_kanalu; nazwa bit error rate; nadmiar bitowy; parametry kanalu; parametry kodera]
 
 results = []
 file_list = glob.glob(os.path.join(output_directory, '*'))
-count_state = 0
+count_state = 1
 
 for file_path in file_list:
     with open(file_path, 'r') as file:
@@ -85,11 +87,11 @@ for file_path in file_list:
             exceed.append(float(tokens[3]))
 
         canal_name = tokens[1]
-        print(file_path)
+        state = 0 if count_state % 2 == 0 else 1
         coder_name = re.split(r'[/.]', file_path)[1][:-2]
         average_ber = sum(ber) / len(ber)
         average_exceed = sum(exceed) / len(exceed) -100 # jezeli jest wiecej w 2 razy to jest wiecej w 100 procent a nie 200
-        state = 0 if count_state % 2 == 0 else 1
+
         count_state += 1
 
         result = {'coder name': coder_name,
@@ -98,8 +100,6 @@ for file_path in file_list:
                   'average exceed': average_exceed,
                   'state': state}
         results.append(result)
-
-        print(results)
 
 x_BSC_good = []
 x_BSC_bad = []
@@ -137,7 +137,7 @@ for result in results:
 plt.figure(figsize=(10, 8))
 plt.scatter(x_BSC_good, y_BSC_good, color='green', label='BST Good State')
 for i, txt in enumerate(titles_BSC_good):
-    plt.annotate(txt, (x_BSC_good[i], y_BSC_good[i]))
+    plt.annotate(txt.replace('_', ' '), (x_BSC_good[i], y_BSC_good[i]))
 plt.xlabel('Average BER')
 plt.ylabel('Average Exceed')
 plt.title('BST Good State')
@@ -149,7 +149,7 @@ plt.show()
 plt.figure(figsize=(10, 8))
 plt.scatter(x_BSC_bad, y_BSC_bad, color='red', label='BST Bad State')
 for i, txt in enumerate(titles_BSC_bad):
-    plt.annotate(txt, (x_BSC_bad[i], y_BSC_bad[i]))
+    plt.annotate(txt.replace('_', ' '), (x_BSC_bad[i], y_BSC_bad[i]))
 plt.xlabel('Average BER')
 plt.ylabel('Average Exceed')
 plt.title('BST Bad State')
@@ -161,7 +161,7 @@ plt.show()
 plt.figure(figsize=(10, 8))
 plt.scatter(x_Gilbert_good, y_Gilbert_good, color='blue', label='Gilbert-Elliot Good State')
 for i, txt in enumerate(titles_Gilbert_good):
-    plt.annotate(txt, (x_Gilbert_good[i], y_Gilbert_good[i]))
+    plt.annotate(txt.replace('_', ' '), (x_Gilbert_good[i], y_Gilbert_good[i]))
 plt.xlabel('Average BER')
 plt.ylabel('Average Exceed')
 plt.title('Gilbert-Elliot Good State')
@@ -173,7 +173,7 @@ plt.show()
 plt.figure(figsize=(10, 8))
 plt.scatter(x_Gilbert_bad, y_Gilbert_bad, color='orange', label='Gilbert-Elliot Bad State')
 for i, txt in enumerate(titles_Gilbert_bad):
-    plt.annotate(txt, (x_Gilbert_bad[i], y_Gilbert_bad[i]))
+    plt.annotate(txt.replace('_', ' '), (x_Gilbert_bad[i], y_Gilbert_bad[i]))
 plt.xlabel('Average BER')
 plt.ylabel('Average Excess')
 plt.title('Gilbert-Elliot Bad State')
@@ -181,8 +181,8 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-for file_path in file_list:
-    os.remove(file_path)
+# for file_path in file_list:
+#     os.remove(file_path)
 
 
 
